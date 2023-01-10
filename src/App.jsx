e0c0c3729fcee53a2eae9fd7867da01aa4d0bfe9
Jose Luis Elizondo <elizondostudios@gmail.com>
2023-01-10 15:18:37 -0600

    App now holds state of all questions

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [gameStart, setGameStart]= React.useState(false)
   const [questions, setQuestions]= React.useState([])
   const [gameOver, setGameOver]= React.useState(false)
+  const [answers, setAnswer]= React.useState([])
 
   function toggleStart(){
     setGameStart(prevGameStart => !prevGameStart)
@@ -19,20 +20,55 @@ function App() {
     setGameOver(prevGameOver => !prevGameOver)
   }
 
+  function findAnswer(question){
+    if(!answers)
+      return ""
+    for(let i= answers.length-1; i>=0; i--){
+      if(answers[i].question===question){
+        return answers[i].answer
+      }
+    }
+    return ""
+  }
+
+  function changeAnswer(question, answer, correct){
+    setAnswer(prevAnswer =>{
+      if(!prevAnswer)
+        return [{question: question, answer: answer, correct: correct}]
+      if(prevAnswer.some(e => e.question===question))
+        return prevAnswer.map(e => e.question===question? {...e, answer: answer}: e)
+      return [...prevAnswer, {question: question, answer: answer, correct: correct}]
+    })
+  }
+
+  const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
+  }
+
   React.useEffect(()=>{
     fetch("https://opentdb.com/api.php?amount=5&type=multiple")
             .then(res => res.json())
             .then(data => setQuestions(data.results))
   },[])
 
+
   const questionElements= questions.map(e =>
-     <Question 
-      question={e.question} 
-      correct={e.correct_answer}
-      incorrect={e.incorrect_answers}
-      key={nanoid()}
-      />
-    )
+      <Question 
+       question={e.question} 
+       correct={e.correct_answer}
+       options={shuffleArray([...e.incorrect_answers, e.correct_answer])}
+       key={nanoid()}
+       answer={findAnswer(e.question)}
+       changeAnswer={changeAnswer}
+       />
+  )
+
 
   return (
     <div className="App">
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,28 +2,15 @@ import { nanoid } from "nanoid";
 import React from "react";
 
 function Question (props){
-    const [answer, setAnswer]= React.useState("")
     
     function handleClick(event){
-        setAnswer(event.target.value)
+        props.changeAnswer(props.question, event.target.value, props.correct)
     }
     
-    const shuffleArray = array => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            const temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-        return array
-    }
-    
-    const [questions, setQuestions]= React.useState(shuffleArray([props.correct, ...props.incorrect]))
-
-    const buttons= questions.map(e => 
+    const buttons= props.options.map(e => 
         <button
          key={nanoid()}
-         className={answer== e? "question--buttons--selected":"question--buttons--unselected"} 
+         className={props.answer== e? "question--buttons--selected":"question--buttons--unselected"} 
          value={e}
          onClick={handleClick}
         >
