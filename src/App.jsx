a772de453249df6b2e2d3c85cf85d32f6f3d5076
Jose Luis Elizondo <elizondostudios@gmail.com>
2023-02-05 12:50:27 -0600

    changed difficulty settings

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
   }
 
   React.useEffect(()=>{
-    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
             .then(res => res.json())
             .then(data => {
               data.results.forEach(e => 
